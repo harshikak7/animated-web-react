bb36539b98b38ce5eb497fb5976ed390e3100fe5
harshika <harshikakolekar84@gmail.com>
2026-04-06 11:55:17 +0530

    updating

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Video from './Video'
 
 const HomeHeroText = () => {
   return (
-    <div>HomeHeroText</div>
+    <div className='font-[font2] pt-5 text-center text-white'>
+      <div className='text-[9vw] justify-center flex items-center uppercase leading-[8vw]'>
+        L'étincelle
+      </div>
+      <div className='text-[9vw] justify-center flex items-center uppercase leading-[8vfw]'>
+        qui
+        <div className='h-[8vw] w-[14vw] rounded-full mt-4 overflow-hidden '>
+          <Video/>
+        </div>
+        génère
+      </div>
+      <div className='text-[9vw] justify-center flex items-center uppercase leading-[10vw]'>
+        la créativité
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Video from '../components/home/Video'
+import HomeHeroText from '../components/home/HomeHeroText'
+import HomeBottom from '../components/home/HomeBottom'
 
 const Home = () => {
   return (
@@ -7,8 +9,9 @@ const Home = () => {
       <div className="h-screen w-screen fixed">
         <Video />
       </div>
-      <div className='h-screen w-screen relative'>
-        
+      <div className='h-screen w-screen relative flex flex-col'>
+        <HomeHeroText />
+        <HomeBottom />
       </div>
     </div>
   )
