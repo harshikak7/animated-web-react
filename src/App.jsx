bbe2da61de6de63ec2d51f0c85175a3b0ff43dab
harshikak7 <harshikakolekar184@gmail.com>
2026-04-23 18:29:46 +0530

    NavBar animation using GSAP initialized

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Agents from './pages/Agents'
+import Navbar from './components/Navigation/Navbar'
 
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
---
+++ src/components/Navigation/FullScreenNav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div>
+      <div>
+        
+        <div>
+           <h1>Projects</h1>
+           <div>
+                <h2>Project 1</h2>
+                <img src="assets/project1.jpg" alt="Project 1" />
+                <h2>Project 1</h2>
+                <img src="assets/project1.jpg" alt="Project 1" />
+                <h2>Project 1</h2>
+                <img src="assets/project1.jpg" alt="Project 1" />
+           </div>
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
